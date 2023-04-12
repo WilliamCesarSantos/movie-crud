71bbb3081326e88f6f982870d9d0ee18ec0a3736
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-04-11 21:07:00 -0300

    Update

--- src/main/java/br/ada/crud/movie/MovieRestController.java
+++ src/main/java/br/ada/crud/movie/MovieRestController.java
@@ -1,5 +1,12 @@
 package br.ada.crud.movie;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -18,6 +25,29 @@ public class MovieRestController {
         this.service = service;
     }
 
+    @Operation(summary = "Create new movie")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Movie was created",
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(oneOf = Movie.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad request",
+                    content = {
+                            @Content(
+                                    examples = {
+                                            @ExampleObject(value = "{\"errors\": [{\"rating\": \"must be less than or equal to 10\"}],\"code\": \"BAD_REQUEST\"}")
+                                    }
+                            )
+                    })
+    })
     @PostMapping(
             consumes = {MediaType.APPLICATION_JSON_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE}
@@ -27,6 +57,21 @@ public class MovieRestController {
         return service.insert(movie);
     }
 
+    @Operation(summary = "List all movies")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Return all movies was registered",
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(
+                                            schema = @Schema(oneOf = Movie.class)
+                                    )
+                            )
+                    }
+            )
+    })
     @GetMapping(
             produces = {MediaType.APPLICATION_JSON_VALUE}
     )
@@ -34,6 +79,29 @@ public class MovieRestController {
         return service.list();
     }
 
+    @Operation(summary = "Find movie by id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Movie was found",
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(oneOf = Movie.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Movie was not found",
+                    content = {
+                            @Content(
+                                    examples = {
+                                            @ExampleObject(value = "{\"message\": \"Not found\", \"code\": \"NOT_FOUND\"}")
+                                    }
+                            )
+                    })
+    })
     @GetMapping(
             produces = {MediaType.APPLICATION_JSON_VALUE},
             value = "/{id}"
@@ -43,6 +111,42 @@ public class MovieRestController {
                 .orElseThrow(EntityNotFoundException::new);
     }
 
+    @Operation(summary = "Update movie")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Movie was edited",
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(oneOf = Movie.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Movie was not found",
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    examples = {
+                                            @ExampleObject(value = "{\"message\": \"Not found\", \"code\": \"NOT_FOUND\"}")
+                                    }
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad request",
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    examples = {
+                                            @ExampleObject(value = "{\"errors\": [{\"rating\": \"must be less than or equal to 10\"}],\"code\": \"BAD_REQUEST\"}")
+                                    }
+                            )
+                    })
+    })
     @PutMapping(
             consumes = {MediaType.APPLICATION_JSON_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE},
@@ -53,6 +157,24 @@ public class MovieRestController {
         return service.insert(movie);
     }
 
+    @Operation(summary = "Delete movie")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Movie was deleted"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Movie was not found",
+                    content = {
+                            @Content(
+                                    examples = {
+                                            @ExampleObject(value = "{\"message\": \"Not found\", \"code\": \"NOT_FOUND\"}")
+                                    }
+                            )
+                    }
+            )
+    })
     @DeleteMapping(
             value = "/{id}"
     )
